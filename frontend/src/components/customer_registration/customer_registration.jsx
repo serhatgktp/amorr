ffe43f08a42cd93069babd3db8acbe4153ed3cc6
Christopher Nathanael <chrisnathan20@gmail.com>
2022-06-06 14:20:45 -0400

    initialized css from jsx for registration form

--- frontend/src/components/customer_registration/customer_registration.jsx
+++ frontend/src/components/customer_registration/customer_registration.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import useStyles from './styles';
+import "./customer_registration.css";
 
 const CustomerRegistration = () => {
 
-  const classes = useStyles();
-
   return(
-    <body className={classes.body}>
-      <span>Hello Senon</span>
+    <body>
+      <div className='registration_form'><span className='new_to'>New to <font color="#d46f5e">a</font><font color="#345392">morr</font>?</span><span className='customer_text'> I am a Customer</span>
+      
+      </div>
     </body>
   );
 };
--- frontend/src/components/customer_registration/styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@mui/styles';
-
-export default makeStyles(() => ({
-    body: {
-        fontFamily: "poppins",
-        fontWeight: 500,
-        fontSize: 64
-    }
-}));
