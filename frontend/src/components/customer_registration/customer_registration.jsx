b402854f05e193e65d0cc53d5f5b715dfa55f867
Christopher Nathanael <chrisnathan20@gmail.com>
2022-06-07 18:17:07 -0400

    finalized styling of customer registration form

--- frontend/src/components/customer_registration/customer_registration.jsx
+++ frontend/src/components/customer_registration/customer_registration.jsx
@@ -7,14 +7,14 @@ const CustomerRegistration = () => {
     <body>
       <div className='registration_form'><span className='new_to'>New to <font color="#d46f5e">a</font><font color="#345392">morr</font>?</span><span className='customer_text'> I am a Customer</span>
       <form className="inputs" method="post">
-                    <label></label><input type="text" placeholder="Email Address" />
+                    <label></label><input type="text" placeholder="Email Address" className="left"/>
                     <label><input type="text" placeholder="Full Name" /></label><br />
-                    <label><input type="password" placeholder="Password" /></label>
-                    <label><input type="repassword" placeholder="Re-enter password" /></label><br />
-                    <label><input className="Address" type="text" placeholder="Address, City, Province, Postal Code" /></label>
+                    <label><input type="password" placeholder="Password" className="left"/></label>
+                    <label><input type="password" placeholder="Re-enter password" /></label><br />
+                    <label><input type="text" placeholder="Address" className="left" id="address"/></label>
                     <label className="idLabel">Attach ID: <input className="id" type="file" id="myFile" name="filename" /></label>
-                    <p>By clicking SIGN UP, I agree to amorr's <a>term's and conditions</a> and <a>privacy policy</a></p> 
-                <input className="submit" type="submit" value="Submit" />
+                    <div><p className="terms">By clicking SIGN UP, I agree to amorr's <a>term's and conditions</a> and <a>privacy policy</a></p></div><br /> 
+                <br/><input className="submit" type="submit" value="SIGN UP" />
       </form>
       </div>
 
