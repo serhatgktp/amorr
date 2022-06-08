a53365f1c2c3b81c7a01783e9189b3fb28b5c3af
theor <theoragavrila@gmail.com>
2022-06-08 18:30:56 -0400

    Fixed minor typos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-// import React, { useState, useEffect } from 'react'
+import React from 'react';
 import './App.css';
 import Home from './components/home/Home';
 
--- frontend/src/components/home/Dashboard/CardItem.js
+++ frontend/src/components/home/Dashboard/CardItem.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './CardItem.css'
+import React from 'react';
+import './CardItem.css';
 
 function CardItem(props) {
 
