1dcb8af45406a1df020630198dda9407f8d042cf
theor <theoragavrila@gmail.com>
2022-06-07 20:04:00 -0400

    Added background colour and font for whole app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+// import React, { useState, useEffect } from 'react'
+import './App.css';
+import Home from './pages/Home';
 
 function App(){
-
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/testing").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
-  return(
-    <div>
-      {(typeof data.testing === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        data.testing.map((testing, i) => (
-          <p key={i}>{testing}</p>
-        ))
-      )}
-    </div>
+  return (
+    <>
+      <Home></Home>
+    </>
   )
 }
 
