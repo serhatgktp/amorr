c3c171b4d00574dd641f2e274a9f9bce231f84f2
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-06-08 16:23:25 -0400

    initialized components and assets folder, changed button color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,11 @@
 import React, { useState, useEffect } from 'react'
-
+import InitialLogin from './components/initial-login/initial-login'
 function App(){
 
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/testing").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return(
-    <div>
-      {(typeof data.testing === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        data.testing.map((testing, i) => (
-          <p key={i}>{testing}</p>
-        ))
-      )}
-    </div>
+    <body>
+      <InitialLogin />
+    </body>
   )
 }
 
