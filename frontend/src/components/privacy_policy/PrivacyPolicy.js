7e214b61c5b90487dad6d12a413c325112eca03e
jwfanous <jwfanous07@gmail.com>
2022-06-09 18:15:21 -0400

    finalized privacy policy page

--- frontend/src/components/privacy_policy/PrivacyPolicy.js
+++ frontend/src/components/privacy_policy/PrivacyPolicy.js
@@ -1,5 +1,5 @@
-
-import "./App.css";
+import React from 'react';  
+import "./PrivacyPolicy.css";
 
 function App() {
   return (
@@ -24,24 +24,144 @@ function App() {
               <span class="ToS">Terms of Service</span>
             </h1>
           </div>
+          <h2 class="first">
+            1. amorr connects Service Providers and Clients
+          </h2>
           <p class="p1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
             ullamcorper ipsum erat, nec bibendum turpis venenatis eget. Maecenas
             et rutrum arcu. Cras vehicula mollis lobortis. Curabitur quis velit
-            id augue porta venenatis. Quisque interdum, tellus eu cursus
+            id augue porta venenatis.
+          </p>
+          <h2 class="second">
+            2. amorr Requires Background Checks
+          </h2>
+          <p class="p2">
+            Quisque interdum, tellus eu cursus
             ultricies, sem enim sagittis sapien, mattis commodo arcu nulla at
             arcu. Ut sit amet rutrum dolor. Nulla tristique enim libero. Aliquam
             erat volutpat.
           </p>
-          <p class="p2">
+          <h2 class="third">
+            3. Billing and payment 
+          </h2>
+          <p class="p3">
             Maecenas luctus ultricies nibh eget ultrices. Fusce porttitor
             fringilla pharetra. Donec et maximus felis. Quisque interdum eros
             vitae magna aliquam blandit. Nunc eget urna eu neque lacinia
-            pulvinar. Aliquam erat volutpat. Vestibulum non rutrum quam, sed
+            pulvinar. Aliquam erat volutpat. 
+            </p>
+        <h2 class="fourth">
+            4. Account Security and Passwords
+          </h2>
+            <p class="p4">
+            Vestibulum non rutrum quam, sed
             commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
             sit amet lectus. Nulla posuere tristique purus id fermentum.
             Maecenas vitae ligula sit amet nisl fringilla aliquet.
-          </p>
+            </p>
+
+            <h2 class="fifth">
+            5. Copyright
+          </h2>
+            <p class="p5">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="sixth">
+            6. Confidential Information
+          </h2>
+            <p class="p6">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="seventh">
+            7. Telephone Communication
+          </h2>
+            <p class="p7">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="eighth">
+            8. Worker Classification
+          </h2>
+            <p class="p8">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="ninth">
+            9. Media and Content
+          </h2>
+            <p class="p9">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="tenth">
+            10. Warranty Disclaimer
+          </h2>
+            <p class="p10">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="eleventh">
+        11. Disputes
+          </h2>
+            <p class="p11">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="twelvth">
+            12. Licensing
+          </h2>
+            <p class="p12">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="thirteenth">
+            13. Consent of Services
+          </h2>
+            <p class="p13">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+            <h2 class="fourteenth">
+            14. Fees and Services
+          </h2>
+            <p class="p14">
+            Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+            </p>
+
+          
           <div
             style={{
               flexDirection: "row",
