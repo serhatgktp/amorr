b572537b0da7fa86ca9fb2cef6a15c500ca9e7b2
MirSafwatAyser <mirsafwatayser@gmail.com>
2022-06-08 20:47:16 -0400

    Added a navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,12 @@
-import React, { useState, useEffect } from 'react'
-
-function App(){
-
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/testing").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
-  return(
+import "./App.css";
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+function App() {
+  return (
     <div>
-      {(typeof data.testing === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        data.testing.map((testing, i) => (
-          <p key={i}>{testing}</p>
-        ))
-      )}
+      <Navbar/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
