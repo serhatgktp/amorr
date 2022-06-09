a75f531a195f2587f5a009b14718aff40ebcedc7
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-06-09 03:09:38 -0400

    modified buttons and input styling for login card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import ServiceProviderRegistration from './components/service_provider_registration/service_provider_registration';
-
+import Login from './components/login-page/login-page'
 const App = () => {
   return(
     <div>
-      <ServiceProviderRegistration/>
+      <Login/>
     </div>
   )
 }
