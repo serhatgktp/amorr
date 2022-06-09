cce0306d147a12d957a01b876071d021e7322a77
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-06-08 23:29:53 -0400

    reformatted page and added media queries to accomodate for different screen resolutions

--- frontend/src/components/service_provider_registration/service_provider_registration.jsx
+++ frontend/src/components/service_provider_registration/service_provider_registration.jsx
@@ -50,7 +50,7 @@ const ServiceProviderRegistration = () => {
     return(
         <body>
 
-        <div className="registration">
+        <section className="registration">
           <form>
               <div className="heading"><span className="newTo">Welcome to <font color="#d46f5e">a</font><font color="#345392">morr</font>! </span>
                 <span className="provide">I provide the following services</span>
@@ -65,7 +65,7 @@ const ServiceProviderRegistration = () => {
                         onChange={handleChange}
 
                         // temporary workaround weird bug where dropdown label is not spaced properly
-                        input={<OutlinedInput sx={{borderRadius: 10, pt: 0, height:55}} label="ListServices"/>}
+                        input={<OutlinedInput sx={{borderRadius: 10, pt: 0, height:55}} label="Types of Services Provided"/>}
                         renderValue={(selected) => selected.join(', ')}
                         MenuProps={MenuProps}
                         >
@@ -79,18 +79,33 @@ const ServiceProviderRegistration = () => {
                     </FormControl>
                 </span>
               </div>
+
               <div className="inputs">
-                  <label></label><input type="text" placeholder="Email Address" className='left'/>
-                  <label><input type="text" placeholder="Full Name"/></label><br />
-                  <label><input type="password" placeholder="Password" className="left"/></label>
-                  <label><input type="password" placeholder="Re-enter password" /></label><br />
-                  <label><input type="text" placeholder="Address" className="left"/></label>
-                  <label className="idLabel">Attach ID: <input className="id" type="file" id="myFile" name="filename" /></label>
-                  <p className='terms'>By clicking SIGN UP, I agree to amorr's <a href='#'>term's and conditions</a> and <a href='#'>privacy policy</a></p>
-                  <input className="submit" type="submit" value="SIGN UP" />
-              </div>   
+                <div className='row'>
+                  <div className="input"><input type="text" placeholder="Email Address"/></div>
+                  <div className="input"><input type="text" placeholder="Full Name"/></div>
+                </div>
+
+                <div className='row'>
+                  <div className="input"><input type="password" placeholder="Password"/></div>
+                  <div className="input"><input type="password" placeholder="Re-enter password"/></div>
+                </div>
+
+                <div className='row'>
+                  <div className="input" id="address"><input type="text" placeholder="Address"/></div>
+                  <div className="input" id="attachFile">
+                    <label for="myFile">Attach ID: </label>
+                    <input className="id" type="file" id="myFile" name="filename"/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="cardFooter">
+                <span className='terms'>By clicking SIGN UP, I agree to amorr's <a href='#'>term's and conditions</a> and <a href='#'>privacy policy</a></span>
+                <input className="submit" type="submit" value="SIGN UP" />
+              </div>  
           </form>
-        </div>
+        </section>
         <div className='tools'></div>
         </body>
     );
