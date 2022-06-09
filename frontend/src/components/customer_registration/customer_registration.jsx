b842dd6af1cb96869489cbb16e3d6b2c84cfa3f0
Christopher Nathanael <chrisnathan20@gmail.com>
2022-06-09 01:21:31 -0400

    implemented new styling from service provider page

--- frontend/src/components/customer_registration/customer_registration.jsx
+++ frontend/src/components/customer_registration/customer_registration.jsx
@@ -5,22 +5,41 @@ const CustomerRegistration = () => {
 
   return(
     <body>
-      <div className='registration_form'><span className='new_to'>New to <font color="#d46f5e">a</font><font color="#345392">morr</font>?</span><span className='customer_text'> I am a Customer</span>
-      <form className="inputs" method="post">
-                    <label></label><input type="text" placeholder="Email Address" className="left"/>
-                    <label><input type="text" placeholder="Full Name" /></label><br />
-                    <label><input type="password" placeholder="Password" className="left"/></label>
-                    <label><input type="password" placeholder="Re-enter password" /></label><br />
-                    <label><input type="text" placeholder="Address" className="left" id="address"/></label>
-                    <label className="idLabel">Attach ID: <input className="id" type="file" id="myFile" name="filename" /></label>
-                    <div><p className="terms">By clicking SIGN UP, I agree to amorr's <a>term's and conditions</a> and <a>privacy policy</a></p></div><br /> 
-                <br/><input className="submit" type="submit" value="SIGN UP" />
-      </form>
-      </div>
 
+        <section className="registration">
+          <form>
+              <div className="heading"><span className="newTo">Welcome to <font color="#d46f5e">a</font><font color="#345392">morr</font>! </span>
+                <span className="provide">I am a new Customer</span>
+              </div>
 
-      <div className='tools'></div>
-    </body>
+              <div className="inputs">
+                <div className='row'>
+                  <div className="input"><input type="text" placeholder="Email Address"/></div>
+                  <div className="input"><input type="text" placeholder="Full Name"/></div>
+                </div>
+
+                <div className='row'>
+                  <div className="input"><input type="password" placeholder="Password"/></div>
+                  <div className="input"><input type="password" placeholder="Re-enter password"/></div>
+                </div>
+
+                <div className='row'>
+                  <div className="input" id="address"><input type="text" placeholder="Address"/></div>
+                  <div className="input" id="attachFile">
+                    <label for="myFile">Attach ID: </label>
+                    <input className="id" type="file" id="myFile" name="filename"/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="cardFooter">
+                <span className='terms'>By clicking SIGN UP, I agree to amorr's <a href='#'>term's and conditions</a> and <a href='#'>privacy policy</a></span>
+                <input className="submit" type="submit" value="SIGN UP" />
+              </div>  
+          </form>
+        </section>
+        <div className='tools'></div>
+        </body>
   );
 };
 
