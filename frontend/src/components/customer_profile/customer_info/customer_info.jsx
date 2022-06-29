eed44cc597bbce4b8019d6620031425038282882
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-06-29 15:49:19 -0400

    added avatar for profile photo and editable address component in editable.jsx file

--- frontend/src/components/customer_profile/customer_info/customer_info.jsx
+++ frontend/src/components/customer_profile/customer_info/customer_info.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React , { useState }from 'react';
 import './styles.css'
 import { Icon, InlineIcon } from '@iconify/react';
-import { Rating } from '@mui/material';
+import { Rating, Avatar } from '@mui/material';
+import customer from "../../../assets/customer_profile/customer.jpg";
+import InlineEdit from "./editable"
 
 const CustomerInfo = () => {
+
+    const address = "100 Lorem Ipsum Road - M1C 0B7"
+    const [value, setValue] = useState(address);
+
     return(
         <div id="customer_info">
-
+            <div id="customer_left">
+                <div><Avatar alt="Sans Calibri" src={customer} sx={{ width: 100, height: 100 }}/></div>
+                <div id="change_photo">Change profile photo</div>
+            </div>
             <div id="customer_name">Sans Calibri</div>
             <div id="rating">
-                <span className="rating_num">4.0</span>
+                <span className="rating_num">4.5</span>
                 <Rating name="read-only" size="medium" value={4.5} precision={0.5} readOnly />
                 <span className="rating_num">(50)</span>
             </div>
             <div> <span>75 </span>Services Ordered</div>
-            
+            <InlineEdit value={value} setValue={setValue}/>
         </div>
     )
 }
