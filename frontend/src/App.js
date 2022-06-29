85f877c147c3a2abfccc75bc0c195e3065dc49ce
theor <theoragavrila@gmail.com>
2022-06-29 10:43:57 -0400

    110: Success popup container with styling created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js'
 import Home from './components/home/Home';
+import SuccessPopup from './components/success_registration_popup/SuccessPopup.js';
 
 function App(){
   return (
     <body>
     <Navbar/>
-    <Home/>
+    <SuccessPopup/>
     </body>
   )
  }
