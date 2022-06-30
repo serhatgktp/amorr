f615d93399b1e2ba94526e5b47984115f5c05caf
theor <theoragavrila@gmail.com>
2022-06-30 18:44:14 -0400

    114: Initialized files of success popup for login

---
+++ frontend/src/components/success_login_popup/SuccessLoginPopup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './SuccessLoginPopup.css'
+import { Icon } from '@iconify/react';
+
+function SuccessLoginPopup(props) {
+  return (props.trigger) ? ( // if triggered, it will display this popup
+    <div className="successPopup">
+        <div className="overlay"></div>
+        <div className="sucessPopupContent">
+            <div className="popupUpper">
+              <Icon id="successIcon" icon="icon-park-outline:check-one"/>
+            </div>
+            <div className="popupLower">
+              <h1 id="successHeading">Login Successful!</h1>
+              <p id="successPara">Please wait while redirecting...</p>
+            </div>
+        </div>
+  </div>
+  ) : ""; // else it will show nothing
+}
+
+export default SuccessLoginPopup
