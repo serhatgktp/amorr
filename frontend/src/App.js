9df4d309f04c61c6cce3899aea773a84f38574f3
theor <theoragavrila@gmail.com>
2022-06-29 20:56:05 -0400

    110: Added props & popup to Customer Registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js'
-import Home from './components/home/Home';
-import SuccessPopup from './components/success_registration_popup/SuccessPopup.js';
+import CustomerRegistration from './components/customer_registration/customer_registration';
 
 function App(){
   return (
     <body>
-    <Navbar/>
-    <SuccessPopup/>
+      <Navbar/>
+      <CustomerRegistration/>
     </body>
   )
  }
