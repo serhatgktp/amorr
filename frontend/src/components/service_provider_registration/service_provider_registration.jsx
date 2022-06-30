58bfaceaec5230c49295a97fafb7a9ff3a468d44
theor <theoragavrila@gmail.com>
2022-06-29 21:11:28 -0400

    110: Added popup to Service Provider Registration

--- frontend/src/components/service_provider_registration/service_provider_registration.jsx
+++ frontend/src/components/service_provider_registration/service_provider_registration.jsx
@@ -10,6 +10,8 @@ import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 
+import SuccessPopup from '../success_registration_popup/SuccessPopup';
+
 const ITEM_HEIGHT = 45;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -49,7 +51,10 @@ const ServiceProviderRegistration = () => {
   const [formValues, setFormValues] = useState(initialValues);
   const initialErrors = { email: false, name: false, password: false, repassword: false, address: false};
   const [formErrors, setFormErrors] = useState(initialErrors);
-
+  
+  // Initial Settings for Success Popup is FALSE, so the Success Popup is not rendered
+  const [triggerSuccessPopup, setTriggerSuccessPopup] = useState(false);
+  
   const handleChanges = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
@@ -58,6 +63,8 @@ const ServiceProviderRegistration = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
+    // If registered correctly, render Success Popup
+    setTriggerSuccessPopup(true);
   };
   
   const validate = (values) => {
@@ -192,6 +199,7 @@ const ServiceProviderRegistration = () => {
           </form>
         </section>
         <div className='tools'></div>
+        <SuccessPopup trigger={triggerSuccessPopup}></SuccessPopup>
         </body>
   );
 }
