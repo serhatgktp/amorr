9dbf422787cbf7fcf54fc2995f146526e7f025a5
aysermir <aysermir@gmail.com>
2022-07-02 20:37:47 -0400

    117: Added routing for the navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -8,17 +8,20 @@ const Navbar = () => {
     <div className="Navbar">
        <h1 class="logo">
                 <span>
-                    <span>a</span>
+                  <a href="/home">
+                  <span>a</span>
                     <span>morr</span>
+                  </a>
+                    
                 </span>
         </h1>
       <div className="nav-items" >
         
-        <a href="/home"><b>About</b></a>
-        <a href="/about"><b>FAQ</b></a>
+        <a href="/about"><b>About</b></a>
+        <a href="/FAQ"><b>FAQ</b></a>
         <a href="/contact"><b>Contact</b></a>       
       </div>
-      <Button variant="contained" sx={{ fontFamily: 'Poppins', color: 'white', backgroundColor: '#d46f5e', borderColor: '#d46f5e' }}>LOG IN / SIGN UP</Button>
+      <Button  variant="contained" sx={{ fontFamily: 'Poppins', color: 'white', backgroundColor: '#d46f5e', borderColor: '#d46f5e' }}> <a href="/initial-signup-landing">LOG IN / SIGN UP</a></Button>
     </div>
   );
 };
