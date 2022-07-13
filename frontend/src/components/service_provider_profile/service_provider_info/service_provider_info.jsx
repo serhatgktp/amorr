effa41050be5d6c7d5907ea03b0ca8534698ffaf
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-07-12 21:58:28 -0400

    154 madd div content into states to accomodate for get request

--- frontend/src/components/service_provider_profile/service_provider_info/service_provider_info.jsx
+++ frontend/src/components/service_provider_profile/service_provider_info/service_provider_info.jsx
@@ -5,11 +5,14 @@ import { Rating, Avatar, requirePropFactory, useScrollTrigger } from '@mui/mater
 
 const ServiceProviderInfo = () => {
 
+    // initializing values
     const [user, setUser] = useState({full_name: "", address: "", total_rating: 0, num_ratings: 0, profile_photo: null});
     const [addr, setAddr] = useState('');
     const [img, setImage] = useState(null);
     const [isEdit, setIsEdit] = useState(false);
+    const [fullName, setFullName] = useState('');
 
+    // dummy get request to get data
     useEffect(() => {
         /*fetch("http://localhost:5000/get-profile", {credentials: 'include'}).then(response =>
           response.json().then(data => {
@@ -19,8 +22,11 @@ const ServiceProviderInfo = () => {
         );*/
         setUser({full_name: "Lorem Ipsum Salons", address: "100 Lorem Ipsum Road - M1C 0B6", total_rating: 0, num_ratings: 0, profile_photo: null})
         setAddr("100 Lorem Ipsum Road - M1C 0B6");
+        setFullName("Lorem Ipsum Salons");
+        setImage(require("../../../assets/profile_photos/4.jpg"))
       }, []);
 
+    // for handling changing profile photo
     const imageHandler = (e) => {
         const selected = e.target.files[0];
 
@@ -34,10 +40,12 @@ const ServiceProviderInfo = () => {
         }
     }
     
+    // changing addr according to input
     const handleChange = (e) => {
         setAddr(e.target.value);
     }
 
+    // exit editing mode when user presses enter or escape
     const onKeyDown = (event) => {
         if (event.key === "Enter" || event.key === "Escape"){
             setIsEdit(false);
@@ -45,6 +53,7 @@ const ServiceProviderInfo = () => {
         }
     }
     
+    // handling clicking save changes button
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsEdit(false);
@@ -80,7 +89,7 @@ const ServiceProviderInfo = () => {
 
             <div id="service_provider_right">
 
-                <div id="service_provider_name">{user.full_name}</div>
+                <div id="service_provider_name">{fullName}</div>
 
                 <div id="rating">
                     <span className="rating_num">4.5</span>
