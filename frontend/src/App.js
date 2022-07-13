ad39b3cff7e8eb940ee1bd6e33764e12268edc19
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-07-13 11:22:41 -0400

    167 initialized customer explore page folder and added sp photo in assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-import CustomerNavbar from './components/Navbar/customerNavbar.js';
-import ServiceProviderNavbar from './components/Navbar/serviceProviderNavbar.js';
-import Navbar from './components/Navbar/Navbar.js';
-import AboutUs from './components/about_us/about_us';
-import ContactUs from './components/contact_us/contact_us';
-import CustomerRegistration from './components/customer_registration/customer_registration';
-import ServiceProviderRegistration from './components/service_provider_registration/service_provider_registration';
-import FAQ from './components/FAQ/FAQpage';
-
-import Home from './components/home/Home';
-import LoginSignupLanding from './components/initial_login_signup_landing/login_signup_landing';
-import InitialLogin from './components/initial-login/initial-login';
-import InitialSignUp from './components/initial-signup/initial-signup';
-import Login from './components/login-page/login-page';
-import CustomerProfile from './components/customer_profile/customer_profile';
-import RequestAppointment from './components/request_appointment/RequestAppointment';
+import CustomerExplore from './components/customer_explore/customer_explore'
 
 import {
   BrowserRouter as Router,
@@ -25,55 +10,10 @@ import {
 } from "react-router-dom";
 
 function App(){
-  
-  const [loggedIn,setLoggedIn] = useState(false); //assuming default value is false
-  const [type,setType] = useState(false);
-  useEffect(() => {
-    fetch("http://localhost:5000/check-user-type", {credentials: 'include'}).then(response =>
-    response.json().then(data => {
-      console.log(data);
-      console.log(data.user_type != "Guest");
-      if (data.user_type == "Guest") {
-        setLoggedIn(false);
-        setType(false);
-      }
-      else if(data.user_type == "Customer"){
-        setType(true);
-        setLoggedIn(true);
-      }else{
-        setType(false);
-        setLoggedIn(true);
-      }
-    })
-    );
-  }, [loggedIn, type]);
 
   return (
     <body>
-    <div>
-    {!loggedIn && !type ? <Navbar/> : ''}
-    {loggedIn && type ? <CustomerNavbar/> : ''}
-    {loggedIn && !type ? <ServiceProviderNavbar/>:''}
-    </div>
-    <Router>
-      <Routes>
-
-        <Route path='/' element={<Home/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<AboutUs/>} />
-        <Route path='/contact' element={<ContactUs/>} />
-        <Route path='/customer-register' element={<CustomerRegistration/>} />
-        <Route path='/sp-register' element={<ServiceProviderRegistration/>} />
-        <Route path='/FAQ' element={<FAQ/>} />
-        <Route path='/initial-signup-landing' element={<LoginSignupLanding/>} />
-        <Route path='/initial-login' element={<InitialLogin/>} />
-        <Route path='/initial-signup' element={<InitialSignUp/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/profile' element={<CustomerProfile/>}/>
-        <Route path='/appointment' element={<RequestAppointment/>}/>
-
-      </Routes>
-    </Router>
+    <CustomerExplore/>
     </body>
   )
  }
---
+++ frontend/src/components/customer_explore/customer_explore.js
@@ -0,0 +1,11 @@
+import React from "react";
+import InteractiveCard from "./interactive_sp_cards/interactive_sp_cards";
+import './customer_explore_styles.css'
+
+const CustomerExplore = () => {
+    return(
+        <InteractiveCard/>
+    )
+}
+
+export default CustomerExplore
