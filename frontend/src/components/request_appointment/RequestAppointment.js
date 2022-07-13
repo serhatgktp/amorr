95511a306b5d8a6c3f9edf6cd3829139079f83d9
theor <theoragavrila@gmail.com>
2022-07-12 20:31:07 -0400

    172: Fixed calendar and display selected date

--- frontend/src/components/request_appointment/RequestAppointment.js
+++ frontend/src/components/request_appointment/RequestAppointment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './RequestAppointment.css'
+import { Icon } from '@iconify/react';
 
 function RequestAppointment() {
     const [date, setDate] = useState(new Date());
@@ -11,8 +12,23 @@ function RequestAppointment() {
     };
 
     return (
-        <div className="RA_dateCalendar">
-            <Calendar onChange={onChange} value={date} />
+        <div className="RA_left">
+            <div className="RA_dateCalendar">
+                <Calendar 
+                    onChange={onChange}
+                    value={date}
+                    maxDate={new Date(2024, 12, 31)} // can only select dates within 2 years
+                    minDate={new Date()} // cannot select dates before today
+                    minDetail='year'
+                />
+            </div>
+            <div class="RA_text">
+                <span className='RA_selectedDateText'>
+                    <Icon id="RA_dateIcon" icon="fontisto:date"/>
+                    Selected Date:{' '}
+                    {date.toDateString()}
+                </span>
+            </div>
         </div>
     )
 }
