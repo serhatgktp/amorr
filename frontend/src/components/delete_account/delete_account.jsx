55d417e6b15dce0f45856ed5e959f166a257570a
axxaxya <annanya21sharma@gmail.com>
2022-07-14 12:21:13 -0400

    175: implemeted frontened for delete

---
+++ frontend/src/components/delete_account/delete_account.jsx
@@ -0,0 +1,48 @@
+// import './Delete_popup'
+
+import * as React from 'react';
+import { useState } from "react"; 
+// import Axios from 'react'; 
+import './Delete_account.css'
+
+const DeleteAccount = () => {
+
+    
+
+    const handleYes= () => {
+
+        fetch('https://jsonplaceholder.typicode.com/posts/1', { method: 'DELETE' })
+        .then(() => this.setState({ status: 'Delete successful' }));
+
+        // fetch('http://localhost:5000/delete-account', {
+        //     method: 'DELETE',
+        //     headers: {"Content-Type": "application/json"},
+        // }).then(response => {
+        //     if (response.ok){
+        //         console.log('User Deleted!'); 
+        //         window.location.reload();
+        //     }else {
+        //         throw new Error(response.statusText)
+        //     }
+        // }).catch(err => {
+        //     console.log(err)
+        // })
+    };
+
+    return(
+        <body className='delete_account_wrapper'>
+            <div className="DAbox">
+                <h1 className='DeleteAccount'> Delete Account! </h1>
+                <p className='sure'> Are you sure you want to delete your account? </p>
+                <p className='sure'> Deleting account will result in losing all your data.</p>
+
+                <button className = 'yes' onClick={handleYes}> <a href='/initial-signup-landing' >Yes</a> </button>
+                <button className='no'> <a href='/home' >No</a></button>
+            </div>
+            
+            {/* <DeletePopup trigger={triggerDeletePopup} /> */}
+        </body>
+    );
+}
+
+export default DeleteAccount; 
