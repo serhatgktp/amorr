e7e59642c8f825286280385c6d2aa4e6ed44695a
theor <theoragavrila@gmail.com>
2022-07-15 15:30:17 -0400

    HOTFIX: Fixed duplicate copy of imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import InitialLogin from './components/initial-login/initial-login';
 import InitialSignUp from './components/initial-signup/initial-signup';
 import Login from './components/login-page/login-page';
 import CustomerProfile from './components/customer_profile/customer_profile';
-import ServiceProviderProfile from './components/service_provider_profile/service_provider_profile'
+import ServiceProviderProfile from './components/service_provider_profile/service_provider_profile';
 import RequestAppointment from './components/request_appointment/RequestAppointment';
 import CustomerExplore from './components/customer_explore/customer_explore.js';
 
@@ -25,7 +25,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import ServiceProviderProfile from './components/service_provider_profile/service_provider_profile.jsx';
 
 function App(){
   
