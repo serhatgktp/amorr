44930f2fd1004f67584d20767d1778c909335839
aysermir <aysermir@gmail.com>
2022-07-15 10:24:03 -0400

    186: Removed the calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ import InitialSignUp from './components/initial-signup/initial-signup';
 import Login from './components/login-page/login-page';
 import CustomerProfile from './components/customer_profile/customer_profile';
 import ServiceProviderProfile from './components/service_provider_profile/service_provider_profile'
-import RequestAppointment from './components/request_appointment/RequestAppointment';
 import CustomerExplore from './components/customer_explore/customer_explore.js';
 
 
@@ -74,8 +73,8 @@ function App(){
         <Route path='/login' element={<Login/>} />
         <Route path='/profile' element={<CustomerProfile/>}/>
 
-        <Route path='/sp-profile' element={<ServiceProviderProfile/>}/>
-        <Route path='/appointment' element={<RequestAppointment/>}/>
+        <Route path='/sp_profile' element={<ServiceProviderProfile/>}/>
+
         <Route path='/explore' element={<CustomerExplore/>}/>
       </Routes>
     </Router>
