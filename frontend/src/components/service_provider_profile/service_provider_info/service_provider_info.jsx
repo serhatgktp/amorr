22eff3d1cc687ba8c24e19c09389588027dd95c0
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-07-15 17:08:53 -0400

    185 uncommented previously written getreq

--- frontend/src/components/service_provider_profile/service_provider_info/service_provider_info.jsx
+++ frontend/src/components/service_provider_profile/service_provider_info/service_provider_info.jsx
@@ -16,7 +16,6 @@ const ServiceProviderInfo = () => {
 
     // dummy get request to get data
     useEffect(() => {
-        /*
         setImage("http://localhost:5000/get-profile-photo")
         fetch("http://localhost:5000/get-sp-profile", {credentials: 'include'}).then(response =>
           response.json().then(data => {
@@ -25,7 +24,7 @@ const ServiceProviderInfo = () => {
             setNumRating(data.num_ratings);
             setRating(data.avg_ratings);
           })
-        );*/
+        );
         setUser({full_name: "Lorem Ipsum Salons", address: "100 Lorem Ipsum Road - M1C 0B6", total_rating: 0, num_ratings: 0, profile_photo: null})
         setAddr("100 Lorem Ipsum Road - M1C 0B6");
         setFullName("Lorem Ipsum Salons");
