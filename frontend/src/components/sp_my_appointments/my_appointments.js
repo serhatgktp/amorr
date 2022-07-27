264b3b5af7a4f2457a7a235f7636aa8d5b8e913d
theor <theoragavrila@gmail.com>
2022-07-27 02:43:02 -0400

    191: Implemented a scrollable card

--- frontend/src/components/sp_my_appointments/my_appointments.js
+++ frontend/src/components/sp_my_appointments/my_appointments.js
@@ -6,8 +6,17 @@ const my_appointments = ( ) => {
     const appt = {name: "Customer Test", price:"32.22", time:"7:00PM", date:"26 July 2022"};
 
     return (
-      <div className="my_appointments">
-          <AppointmentCard appt={appt}/>
+      <div className="sp_my_appointments">
+          <div className="appointment_section">
+            <div className="type_of_appointment_heading">Awaiting Confirmation</div>
+            <div className="list_of_appointments">
+                <AppointmentCard appt={appt}/>
+                <AppointmentCard appt={appt}/>
+                <AppointmentCard appt={appt}/>
+                <AppointmentCard appt={appt}/>
+                <AppointmentCard appt={appt}/>
+            </div>
+          </div>
       </div>
     )
   }
