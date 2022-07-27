ceb6974b9eb79caee434efef56bae5433ef55fad
theor <theoragavrila@gmail.com>
2022-07-27 14:34:31 -0400

    191: Fixed overall styling in my appointments page

--- frontend/src/components/sp_my_appointments/my_appointments.js
+++ frontend/src/components/sp_my_appointments/my_appointments.js
@@ -4,7 +4,7 @@ import './my_appointment.css'
 
 const my_appointments = ( ) => {
     const [waitingApptList, setWaitingApptList] = useState([
-        {name: "Customer 1", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "カワイイ", price:"32.22", time:"7:00PM", date:"26 July 2022"},
         {name: "Customer 2", price:"32.22", time:"7:00PM", date:"26 July 2022"},
         {name: "Customer 3", price:"32.22", time:"7:00PM", date:"26 July 2022"},
         {name: "Customer 4", price:"32.22", time:"7:00PM", date:"26 July 2022"},
@@ -12,17 +12,17 @@ const my_appointments = ( ) => {
     ]);
 
     const [confirmedApptList, setConfirmedApptList] = useState([
-        {name: "Customer 6", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer 7", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer 3", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer 4", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer 5", price:"32.22", time:"7:00PM", date:"26 July 2022"}
+        {name: "A", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "B", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "C", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "D", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "E", price:"32.22", time:"7:00PM", date:"26 July 2022"}
     ]);
 
     const [pastApptList, setPastApptList] = useState([
-        {name: "Customer Wee", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer Woo", price:"32.22", time:"7:00PM", date:"26 July 2022"},
-        {name: "Customer 3", price:"32.22", time:"7:00PM", date:"26 July 2022"}
+        {name: "UwU", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "OwO", price:"32.22", time:"7:00PM", date:"26 July 2022"},
+        {name: "IwI", price:"32.22", time:"7:00PM", date:"26 July 2022"}
     ]);
 
     return (
