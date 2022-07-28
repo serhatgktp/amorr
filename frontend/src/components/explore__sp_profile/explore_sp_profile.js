8cf0312ebadca1160f4e5673c952b8b2ea3fd7fc
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-07-27 22:01:53 -0400

    214 added price list component

--- frontend/src/components/explore__sp_profile/explore_sp_profile.js
+++ frontend/src/components/explore__sp_profile/explore_sp_profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './explore_sp_profile_styles.css'
+import ExploreSpPriceList from "./explore_sp_price_list/explore_sp_price_list";
 import ExploreSpBio from "./explore_sp_profile_bio/explore_sp_profile_bio";
 import ExploreSpInfo from "./explore_sp_profile_info/explore_sp_profile_info";
 
@@ -6,7 +8,10 @@ const ExploreSpProfile = () => {
     return(
         <body>
             <ExploreSpInfo/>
-            <ExploreSpBio/>
+            <div id="service_provider_profile_body_section">
+                <ExploreSpBio/>
+                <ExploreSpPriceList/>
+            </div>
         </body>
     )
 }
