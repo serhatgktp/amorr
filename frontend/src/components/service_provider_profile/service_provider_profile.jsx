09ec9d181693a56685550d19f7342146b6da12f6
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-07-28 12:27:00 -0400

    207 added review card to sp profile and added styling

--- frontend/src/components/service_provider_profile/service_provider_profile.jsx
+++ frontend/src/components/service_provider_profile/service_provider_profile.jsx
@@ -3,6 +3,7 @@ import ServiceProviderBio from "./service_provider_bio/service_provider_bio";
 import ServiceProviderInfo from "./service_provider_info/service_provider_info";
 import ServiceProviderListPrices from "./service_provider_list_prices/service_provider_list_prices";
 import './service_provider_profile.css'
+import ServiceProviderReviewCard from "./service_provider_reviews/service_provider_review";
 
 const ServiceProviderProfile = () => {
     return(
@@ -12,6 +13,7 @@ const ServiceProviderProfile = () => {
                 <ServiceProviderBio/>
                 <ServiceProviderListPrices/>
             </div>
+            <ServiceProviderReviewCard/>
         </body>
     )
 }
