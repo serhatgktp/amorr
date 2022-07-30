5f9d43f020f2e619b3c0a1e2a73bedc108009dd4
aysermir <aysermir@gmail.com>
2022-07-30 02:17:03 -0400

    168: imported useffect from react

--- frontend/src/components/request_appointment/services_provided.js
+++ frontend/src/components/request_appointment/services_provided.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import './services_provided.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle, faMinusCircle, faCircle, faArrowRight} from '@fortawesome/free-solid-svg-icons';
